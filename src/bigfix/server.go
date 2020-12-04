108fe971537ca4bf6eb3303eb631bba052c0d430
Michael Schwarz <mschwarz@multitool.net>
2020-12-04 10:21:45 -0600

    Start building modules

---
+++ src/bigfix/server.go
@@ -0,0 +1,16 @@
+package bigfix
+
+import (
+	"fmt"
+)
+
+type BFServer struct {
+	name string
+	port string
+	username string
+	password string
+}
+
+func URL() int, string {
+	return nil, "https://" + name + ":" + port
+}
