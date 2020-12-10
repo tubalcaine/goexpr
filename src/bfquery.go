e558230de2229f53b35e5afcf2d9f400ae4f19e8
Michael Schwarz <mschwarz@multitool.net>
2020-12-10 10:45:39 -0600

    Call query

--- src/bfquery.go
+++ src/bfquery.go
@@ -10,8 +10,12 @@ func main() {
 
 	server.Name = "10.10.220.60"
 	server.Port = "52311"
+	server.Username = "IEMAdmin"
+	server.Password = "BigFix!123"
 
 	url, _ := bigfix.BaseBFURL(server)
+
+	result, err := bigfix.Query(server, "names of bes computers")
 
 	fmt.Println(url)
 }
