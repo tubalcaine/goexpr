844b067d33cd1d259ffca698b693516bc010bc7e
Michael Schwarz <mschwarz@multitool.net>
2020-12-10 10:39:23 -0600

    Workpoint commit

--- src/bigfix/server.go
+++ src/bigfix/server.go
@@ -1,7 +1,10 @@
 package bigfix
 
 import (
+	"crypto/tls"
+	"io/ioutil"
 	"net/http"
+	"net/url"
 )
 
 // BFServer - This structure represents the definition of a BigFix server
@@ -26,29 +29,41 @@ type BFSrQuery struct {
 
 // BaseBFURL - returns a URL for the BFServer passed in
 func BaseBFURL(srv BFServer) (string, int) {
-	return "https://" + srv.Name + ":" + srv.Port, 0
+	return "https://" + srv.Username + ":" + srv.Password + "@" + srv.Name + ":" + srv.Port, 0
 }
 
 // NewBFSession - Create and return a BFSession for a given BFServer
-func NewBFSession(srv BFServer) (BFSession, int) {
+func NewBFSession(srv BFServer) (*BFSession, error) {
+	var client BFSession
 
+	customTransport := http.DefaultTransport.(*http.Transport).Clone()
+	customTransport.TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
+	cli := &http.Client{Transport: customTransport}
+
+	client.Conn = cli
+	return &client, nil
 }
 
-// SessionQuery - A standalone Session Relevance query using a BFServer. Not
+// Query - A standalone Session Relevance query using a BFServer. Not
 // a persisten session. Useful for rare, one-off queries. Use a session based
 // function when you are doing a lot of operations.
-func SessionQuery(srv BFServer, query BFSrQuery) (string, int) {
-	var url string
+func Query(srv BFServer, query BFSrQuery) (string, error) {
+	var apiurl string
 
-	url, _ = BaseBFURL(srv) + "/api/query"
+	apiurl, _ = BaseBFURL(srv)
+	apiurl = apiurl + "/api/query"
 
-	resp, err := http.PostForm()
+	resp, err := http.PostForm(apiurl, url.Values{"relevance": {query.SessionRelevance}})
+	defer resp.Body.Close()
 
+	result, err := ioutil.ReadAll(resp.Body)
+
+	return string(result), err
 }
 
 // SessionQuery - A session based Session Relevance query using a BFServer.
 // This uses a persisten session. Useful for many queries in a single program
 // or operation.
-func SessionQuery(sess BFSession, query BFSrQuery) (string, int) {
-
+func SessionQuery(sess BFSession, query BFSrQuery) (string, error) {
+	return "", nil
 }
