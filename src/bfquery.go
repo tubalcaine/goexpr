7c7c6f9e67979cb8cdddb47e33ea54debca093fc
Michael Schwarz <mschwarz@multitool.net>
2020-12-13 19:32:45 -0600

    Fix the weird source corruption VSC did.

--- src/bfquery.go
+++ src/bfquery.go
@@ -1,28 +1,16 @@
 package main
 
-import (
-	"bigfix"
-import (
-	"fmt"
-import (
-)
-import (
 func main() {
-
-func main() {
-import (
 	server.Name = "10.10.220.60"
-
-import (
 	server.Port = "52311"
-	"bigfix"	s	"bigfix"erve	server.Password = "BigFix!123".import (Username = "	"bigfix"MAdmin"
-	sere	"bigfix"r.Password=	"bigfix" "Bigi	"bigfix"x!123"
+	server.Username = "IEMAdmin"
+	server.Password = "BigFix!123"
 
-	url, _
-	"bigfix" := bigfix.BaseBFURL(server)
-	"bigfix"
-	srq, _ := bigfix.MakeSrQuery("n	"bigfix"ames of bes computers")
-	"f	s"_ 	"f=g"x.Ne}wBFSession(server)
+	url, _ := bigfix.BaseBFURL(server)
+
+	srq, _ := bigfix.MakeSrQuery("names of bes computers")
+
+	sess, _ := bigfix.NewBFSession(server)
 
 	result, err := bigfix.SessionQuery(sess, srq)
 
